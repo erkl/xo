054c1b3ae081deb619135531554fc3047dde279c
Erik Lundin <erik@kvick.co>
2014-09-05 22:11:20 +0100

    Add the WriteString utility function

--- util.go
+++ util.go
@@ -9,3 +9,31 @@ type readwriter struct {
 	Reader
 	Writer
 }
+
+// WriteString writes a string to w.
+func WriteString(w Writer, s string) (int, error) {
+	var n int
+
+	// If s is larger than w's internal buffer, the write has to be
+	// split into multiple chunks.
+	for len(s) > 0 {
+		buf, err := w.Reserve(len(s))
+		if len(buf) == 0 {
+			if err == nil {
+				err = ErrShortReserve
+			}
+			return n, err
+		}
+
+		nc := copy(buf, s)
+
+		if err := w.Commit(nc); err != nil {
+			return n, err
+		}
+
+		s = s[nc:]
+		n += nc
+	}
+
+	return n, nil
+}
